521f0d00e42d9f58bb794272eda11444c9e8975b
Ronald <ronaldpaul.peter@ebix.com>
2014-02-26 10:00:37 +0530

    Added MainClass.java just for testing Github operations from Eclipse

---
+++ src/com/ronald/struts2/MainClass.java
@@ -0,0 +1,10 @@
+package com.ronald.struts2;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Testing repo clone, commit and push to Github");
+
+	}
+
+}
